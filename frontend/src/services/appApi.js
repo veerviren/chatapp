f4381f3609a4368e11bf2dde01079c7a8ea60512
VIREN VARIYA <77713965+veerviren@users.noreply.github.com>
2023-04-20 14:05:57 +0530

    Update appApi.js
    backend url update

--- frontend/src/services/appApi.js
+++ frontend/src/services/appApi.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const appApi = createApi({
     reducerPath: "appApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:5001",
+        baseUrl: "https://chatapp-j4wl.onrender.com",
     }),
 
     endpoints: (builder) => ({
